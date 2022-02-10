b3b9c806b2aa39355c581956b168d44c9ce413b2
seeeuni <seeun0905@cau.ac.kr>
2022-02-11 00:23:33 +0900

    #1 기본문법

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+
+fun main() {
+    // 한 줄 주석입니다.
+
+    /*
+        여러 줄
+        주석 입니다.
+     */
+
+    print("문자열 입니다")
+    print("문자열 입니다")
+
+    println("문자열입니다")
+    println("문자열입니다")
+
+    println("값 : ${100}")
+
+    println("세미콜론 없음")
+    println("세미콜론 있음")
+
+    println("명령문1");println("명령문2");println("명령문3")
+
+    // 정수 리터럴
+    println(100)
+    println(10000000000)
+    println(10000000000L)
+    //L 붙여도 되고 안붙여도 됨
+    println(938_492_934_823)
+    //세자리수마다 쉽게 구분하기 위함
+
+    // 실수 리터럴
+    println(11.11)
+    println(22.22F)
+    //F는 저장공간 줄이기 위함
+
+    //문자 리터럴
+    println("A")
+
+    //문자열 리터럴
+    println("문자열")
+
+    // 논리 리터럴
+    println(true)
+    println(false)
+
+    // Raw string
+    println("동해물과 백두산이 \n 마르고 닳도록")
+
+
+}
