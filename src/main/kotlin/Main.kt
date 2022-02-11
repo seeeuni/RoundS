c31836ffd8b85047342be0ab9d1c14fb5f4ce102
seeeuni <seeun0905@cau.ac.kr>
2022-02-12 00:23:57 +0900

    #1 함수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -101,5 +101,103 @@ fun main () {
     */
     // var a8:Int = a6!! -> 실행안됨 why? 이미 a6은 null이기 때문
 
+    test1()
+    test2(100,11.11)
+    //값을 넣으면 자동으로 매개변수가 뭔지(a1, a2) 보여줌
+    val k1:Int = 100
+    val k2:Double = 11.11
+    test2(k1, k2)
+
+
+    test2(500, 55.55)
+    test2(a2=66.66, a1=600)
+
+    test3(100,55.55)
+    test3(100)
+    test3(a2=66.66)
+
+
+    val r1:Int = test4(100,200)
+    val r2:Int = test4(1000,2000)
+    println("r1 : $r1")
+    println("r2 : $r2")
+
+    test5()
+    test6()
+    test7()
+    test7(100)
+    test7(11.11)
+    test7(100,200)
+
+    test8()
+    //test9()
+}
+    //함수
+fun test1(){
+    println("test1 호출")
+        println("-------------------------")
+}
+    //매개변수 "변수명:자료형"
+
+fun test2(a1:Int, a2:Double){
+    println("test2 호출")
+    println("a1 : $a1")
+    println("a2 : $a2")
+    println("------------------------")
+
+
+}
+//기본값 설정시 값을 전달하지 않으면 기본 값이 매개변수에 설정됨
+fun test3(a1:Int = 0, a2:Double = 0.0){
+    println("test3이 호출되었습니다")
+    println("a1 : $a1")
+    println("a2 : $a2")
+    println("-------------")
 }
 
+//반환 타입
+fun test4(a1:Int, a2:Int) : Int{
+    val result:Int = a1 + a2
+    return result
+}
+// Unit은 반환할 값이 없다는 의미 -> 생략가능
+fun test5() : Unit {
+    println("test5 호출")
+    println("-----------------------")
+}
+
+fun test6() {
+    println("test6 호출")
+    println("----------------")
+}
+
+//함수의 오버로딩 - 매개변수의 자료형을 다르게해서 같은 이름, 여러함수 정의
+
+fun test7() {
+    println("test7 호출 - 매개변수 없음")
+    println("--------------")
+}
+
+fun test7(a1:Int) {
+    println("test7 호출 - 매개변수 한개(Int)")
+    println("--------------")
+}
+
+fun test7(a1:Double) {
+    println("test7 호출 - 매개변수 한개(Double)")
+    println("--------------")
+}
+
+fun test7(a1:Int, a2:Int){
+    println("test7 호출 - 매개변수 두개")
+    println("---------------------")
+}
+
+//지역함수 - 함수 내에 정의한 함수 / 함수를 정의한 함수 안에서만 호출 가능
+fun test8() {
+    println("test8 호출")
+    fun test9() {
+        println("test9 호출")
+    }
+        test9()
+    }
